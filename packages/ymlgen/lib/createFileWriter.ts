4bf630895a1b639708fb82a7e9371d7c99ff5de8
hung.nguyen <linqtojs@gmail.com>
2022-09-16 15:40:10 +0700

    chore: add skipIfExist option

--- packages/ymlgen/lib/createFileWriter.ts
+++ packages/ymlgen/lib/createFileWriter.ts
@@ -1,16 +1,27 @@
 import fs from "fs";
 import { dirname, resolve } from "path";
 
+export type WriteFileOptions = {
+  skipIfExist?: boolean;
+};
+
 const createFileWriter =
   (
     workingDir: string,
     onSuccess?: (fullPath: string) => void,
     onError?: (error: unknown) => void
   ) =>
-  async (fileName: string, content: string) => {
+  async (
+    fileName: string,
+    content: string,
+    { skipIfExist }: WriteFileOptions = {}
+  ) => {
     try {
       const fullPath = resolve(workingDir, fileName);
       const dir = dirname(fullPath);
+      if (skipIfExist && fs.existsSync(fullPath)) {
+        return;
+      }
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
       }
